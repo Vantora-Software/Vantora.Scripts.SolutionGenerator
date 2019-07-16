6fcbbbfa9e96e7f49ee83df4ddf9504ad81ccf0f
Jeremy Ames <fizch26@gmail.com>
2019-07-15 22:43:00 -0500

    :tada: create power shell script for generating a scaffolded solution

---
+++ sln_gen.ps1
@@ -0,0 +1,19 @@
+﻿param([string]$sln)
+
+if ($sln.Length -gt 0) {
+    dotnet new sln -n $($sln)
+    dotnet new classlib -n $($sln) -o src\library
+    del .\src\library\Class1.cs
+
+    md .\src\WindowsService
+
+    dotnet new xunit -o tests\integration
+    dotnet add tests\integration\integration.csproj reference src\library\$($sln).csproj
+    del .\tests\integration\UnitTest1.cs
+
+    dotnet new xunit -o tests\unit
+    dotnet add tests\unit\unit.csproj reference src\library\$($sln).csproj
+    del .\tests\unit\UnitTest1.cs
+} else {
+    Write-Host "No argument was supplied for the solution name"
+}
